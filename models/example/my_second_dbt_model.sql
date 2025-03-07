b650261466d98fede5ba6f2d5bcdade4ee47235a
Thoushifs1 <thoushifahammad1@gmail.com>
2025-03-07 16:38:01 +0530

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,5 +1,14 @@
 -- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+{{ config(materialized='table') }}
+
+with source_data as (
+    select 100 as id
+    union all
+    select 200 as id
+    union all
+    select 300 as id
+)
+
+select * from source_data
+

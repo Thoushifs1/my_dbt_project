e132cacc1c6f1b6c4d5d4c529661d2fc8bf1f657
Thoushifs1 <thoushifahammad1@gmail.com>
2025-03-19 22:45:33 +0530

    Update schema_changes.sql

--- macros/schema_changes.sql
+++ macros/schema_changes.sql
@@ -1,11 +1,1 @@
-{% macro create_schema() %}
-  {% set schema_name = target.schema %}
-  
-  -- Create schema if it does not exist
-  {% set create_schema_sql %}
-    CREATE SCHEMA IF NOT EXISTS {{ schema_name }};
-  {% endset %}
-  
-  -- Execute the SQL
-  {% do run_query(create_schema_sql) %}
-{% endmacro %}
+
